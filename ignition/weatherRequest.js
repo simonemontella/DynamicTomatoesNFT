3b5104eb51155f193334bdc2519a22749dd95875
simonemontella <simonemontella8@gmail.com>
2025-02-10 00:51:35 +0100

    ignition

---
+++ ignition/weatherRequest.js
@@ -0,0 +1,19 @@
+const response = await Functions.makeHttpRequest({
+  url: `https://api.openweathermap.org/data/2.5/weather`,
+  method: "GET",
+  params: {
+    q: "Naples",
+    appid: secrets.OPENWEATHER_API_KEY
+  }
+});
+
+if (response.error) {
+  throw new Error("Errore nella richiesta a OpenWeatherMap");
+}
+
+const dataResult = {
+    temp: response.data.main.temp,
+    hum: response.data.main.humidity
+}
+
+return Functions.encodeString(JSON.stringify(dataResult))
